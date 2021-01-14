c5e58641b26a435ac077126aeea1cbc9fd0dcf8c
Vaillus <hugovaillaud@gmail.com>
2021-01-14 17:52:51 +0100

    made it packageable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='godot_interface',
+version='0.1',
+description='interface to Godot environments for reinforcement learning applications',
+url='#',
+author='vaillus',
+author_email='hugovaillaud@gmail.com',
+license='MIT',
+packages=find_packages(),
+zip_safe=False
+)
